e973fac53074afdfae6c76109828cefd018522e6
Mohamed <60451644+Mohamed-C0DE@users.noreply.github.com>
2022-03-02 19:57:15 -0500

    add main component

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -4,6 +4,7 @@ import Footer from "../Components/Footer";
 import Header from "../Components/Header";
 import Offers from "../Components/Offers";
 import ScrollToTop from "../Components/ScrollToTop";
+import Main from "../UI/Main";
 import styles from "./Contact.module.css";
 
 const Contact = () => {
@@ -12,9 +13,11 @@ const Contact = () => {
       <ScrollToTop />
       <Offers />
       <Header />
-      <Container fluid>
-        <p className={styles.contact}>Contact Page</p>
-      </Container>
+      <Main>
+        <Container fluid>
+          <p className={styles.contact}>Contact Page</p>
+        </Container>
+      </Main>
       <Footer />
     </div>
   );
