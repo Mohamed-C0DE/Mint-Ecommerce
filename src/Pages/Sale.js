2647120a31075fa7c8b7d6395709a05623107bb0
Mohamed <60451644+Mohamed-C0DE@users.noreply.github.com>
2022-03-02 19:53:33 -0500

    add main component

--- src/Pages/Sale.js
+++ src/Pages/Sale.js
@@ -3,6 +3,7 @@ import Footer from "../Components/Footer";
 import Header from "../Components/Header";
 import Offers from "../Components/Offers";
 import ScrollToTop from "../Components/ScrollToTop";
+import Main from "../UI/Main";
 import styles from "./Sale.module.css";
 
 const Sale = () => {
@@ -11,9 +12,11 @@ const Sale = () => {
       <ScrollToTop />
       <Offers />
       <Header />
-      <Container fluid>
-        <p className={styles.sale}>Sale Page</p>
-      </Container>
+      <Main>
+        <Container fluid>
+          <p className={styles.sale}>Sale Page</p>
+        </Container>
+      </Main>
       <Footer />
     </div>
   );
